67460c842a2b041bf5634e9a28f16a9c69f3a757
AbhishekRavi063 <abhishekravi063@gmail.com>
2025-06-22 23:31:30 +0530

    added port for raillway deployment

--- main.py
+++ main.py
@@ -457,3 +457,9 @@ def ask_question(payload: Question):
         "sources": sources,
         "video_url": video_url
     }
+
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))  # Railway provides PORT
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
